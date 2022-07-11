e8a63f97d5ff3033bb8c8d73ce8e3a7de11cb081
tanvi1017 <tripathi.tanvi1996@gmail.com>
2022-07-11 16:16:04 +0530

    added toast

--- app/src/main/java/com/tanvi/newstap/MainActivity.kt
+++ app/src/main/java/com/tanvi/newstap/MainActivity.kt
@@ -2,10 +2,12 @@ package com.tanvi.newstap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"This is a toast", Toast.LENGTH_LONG)
     }
 }
